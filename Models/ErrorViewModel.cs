f9e25dd23ff032d970dbe534b37f2fb9a2639e0d
zainab-yekta <zeinab.r.yekta@gmail.com>
2025-01-15 22:36:18 +0100

    Initial commit for ASP.NET MVC project setup.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyDashboardApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
